f8c5b6b32966a0f46016b2078d946f5b3c6583da
Rahul Chandak <rahulschandak@gmail.com>
2024-04-15 00:58:48 -0400

    Homepage mobile responsive

--- src/pf-main/pages/homeScreen/home-screen.js
+++ src/pf-main/pages/homeScreen/home-screen.js
@@ -46,7 +46,46 @@ function HomeScreen() {
       </div>
     </div>
   ) : (
-    <div>Home Page Mobile</div>
+    <div className="page-container-mobile">
+      <div className="image-container-mobile">
+        <img
+          src="images\ProfilePic.png"
+          className="img-style"
+          alt="Profile Pic"
+        />
+      </div>
+      <div className="text-container-mobile">
+        <BodyLabel className="h2">Hello World,</BodyLabel>
+        <BodyLabel className="h2">I am Rahul Chandak</BodyLabel>
+        <BodyLabel className="body-text padding-top">
+          A Master's in Computer Science student at NEU
+        </BodyLabel>
+        <div className="social-media-container">
+          <img
+            src="images\instagram.png"
+            className="social-media-style"
+            alt="Instagram"
+          />
+          <img
+            src="images\twitter.png"
+            className="social-media-style"
+            alt="Twitter"
+          />
+          <img
+            src="images\github.png"
+            className="social-media-style"
+            alt="GitHub"
+          />
+          <img
+            src="images\linkedIn.png"
+            className="social-media-style"
+            alt="LinkedIn"
+          />
+        </div>
+        <button className="button-style"> Resume </button>
+      </div>
+      
+    </div>
   );
 }
 export default HomeScreen;
