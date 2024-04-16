47b389f45b4690f89449e0e5551e15e7235765bd
Rahul Chandak <rahulschandak@gmail.com>
2024-04-16 00:53:34 -0400

    skills page

--- src/pf-main/pages/skillsScreen/skills-screen.js
+++ src/pf-main/pages/skillsScreen/skills-screen.js
@@ -1,8 +1,175 @@
-import React, { useContext } from "react";
-import ScreenSizeContext from "../../components/common/screen-size";
+import React from "react";
+import "./skills-screen.css";
+import BodyLabel from "../../components/common/body-label";
 
 function SkillsScreen() {
-  const { isDesktop } = useContext(ScreenSizeContext);
-  return isDesktop ? <div>Skills Page</div> : <div>Skills Page Mobile</div>;
+
+  const heading = "Tools and Frameworks I have worked with:";
+  const programmingLanguagesHeading = "1. Programming Languages";
+  const databaseHeading = "2. Databases";
+  const webTechnologiesHeading = "3. Web Technologies";
+  const toolsHeading = "4. Tools";
+
+  const skillsProgLang = [
+    {
+      id: 1,
+      name: "Java",
+      image: require("./images/Java.png"),
+    },
+    {
+      id: 2,
+      name: "JavaScript",
+      image: require("./images/JS.png"),
+    },
+    {
+      id: 3,
+      name: "TypeScript",
+      image: require("./images/TS.png"),
+    },
+    {
+      id: 4,
+      name: "Python",
+      image: require("./images/Python.png"),
+    },
+    {
+      id: 5,
+      name: "CSharp",
+      image: require("./images/CSharp.png"),
+    },
+  ];
+
+  const skillsDatabase = [
+    {
+      id: 1,
+      name: "MySQL",
+      image: require("./images/MySQL.png"),
+    },
+    {
+      id: 2,
+      name: "PLSQL",
+      image: require("./images/PLSQL.png"),
+    },
+    {
+      id: 3,
+      name: "MongoDB",
+      image: require("./images/MongoDB.png"),
+    },
+  ];
+
+  const skillsTools = [
+    {
+      id: 1,
+      name: "Visual Studio",
+      image: require("./images/VS.png"),
+    },
+    {
+      id: 2,
+      name: "IntelliJ",
+      image: require("./images/IntelliJ.png"),
+    },
+    {
+      id: 3,
+      name: "GIT",
+      image: require("./images/GIT.png"),
+    },
+    {
+      id: 3,
+      name: "GITHUB",
+      image: require("./images/GitHub.png"),
+    },
+    {
+      id: 3,
+      name: "Docker",
+      image: require("./images/Docker.png"),
+    },
+    {
+      id: 4,
+      name: "Kubernetes",
+      image: require("./images/Kubernetes.png"),
+    },
+    {
+      id: 8,
+      name: "Postman",
+      image: require("./images/Postman.png"),
+    },
+    {
+      id: 8,
+      name: "AWS",
+      image: require("./images/AWS.png"),
+    },
+    {
+      id: 6,
+      name: "MySQLWorkbench",
+      image: require("./images/MySQLWorkbench.png"),
+    },
+    {
+      id: 7,
+      name: "Jenkins",
+      image: require("./images/Jenkins.png"),
+    },
+    {
+      id: 8,
+      name: "Axure",
+      image: require("./images/Axure.png"),
+    },
+  ];
+
+  const skillsWebTech = [
+    {
+      id: 1,
+      name: "HTML",
+      image: require("./images/HTML.png"),
+    },
+    {
+      id: 2,
+      name: "CSS",
+      image: require("./images/CSS.png"),
+    },
+    {
+      id: 3,
+      name: "Bootstrap",
+      image: require("./images/Bootstrap.png"),
+    },
+    {
+      id: 4,
+      name: "ReactJS",
+      image: require("./images/ReactJS.png"),
+    },
+    {
+      id: 5,
+      name: "NodeJS",
+      image: require("./images/Node.png"),
+    },
+  ];
+
+  return (
+    <div className="skills-container">
+      <BodyLabel className="h2 heading-style">{heading}</BodyLabel>
+      <BodyLabel className="h3">{programmingLanguagesHeading}</BodyLabel>
+      <div className="skills-style">
+        {skillsProgLang.map((data) => (
+          <img className="image-style" alt={data.name} src={data.image} />
+        ))}
+      </div>
+      <BodyLabel className="h3">{databaseHeading}</BodyLabel>
+      <div className="skills-style">
+        {skillsDatabase.map((data) => (
+          <img className="image-style" alt={data.name} src={data.image} />
+        ))}
+      </div>
+      <BodyLabel className="h3">{webTechnologiesHeading}</BodyLabel>
+      <div className="skills-style">
+        {skillsWebTech.map((data) => (
+          <img className="image-style" alt={data.name} src={data.image} />
+        ))}
+      </div>
+      <BodyLabel className="h3">{toolsHeading}</BodyLabel>
+      <div className="skills-style">
+        {skillsTools.map((data) => (
+          <img className="image-style" alt={data.name} src={data.image} />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default SkillsScreen;
