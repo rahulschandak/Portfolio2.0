36cce03da52af74598a1d494915d81d50d91fcd4
Rahul Chandak <rahulschandak@gmail.com>
2024-05-07 23:29:15 -0400

    adding disclaimer

--- src/pf-main/index.js
+++ src/pf-main/index.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/header/header";
 import Body from "./components/routes/routes";
 import "../App.css";
 import { ScreenSizeProvider } from "./components/common/screen-size";
 import Footer from "./components/footer/footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Portfolio() {
+
+  const [showPopup, setShowPopup] = useState(true);
+  useEffect(() => {
+    const visitedBefore = localStorage.getItem("visitedBefore");
+    if (!visitedBefore) {
+      setShowPopup(true);
+      localStorage.setItem("visitedBefore", "true");
+    }
+  }, []);
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+
   return (
     <ScreenSizeProvider>
       <Header />
-      <hr />
+      <hr className="hr" />
       <Body />
       <Footer />
+      {showPopup && (
+        <div className="popup-overlay">
+          <div className="popup">
+            <p>
+              This website is a ground-up creation, entirely designed from
+              scratch by Rahul Chandak. (View this on a desktop for the best experience).
+            </p>
+            <p>
+              It remains an ongoing project, constantly evolving with frequent
+              updates to enhance its features and functionality.
+            </p>
+            <p>
+              <button className="btn btn-primary" onClick={handlePopupClose}>
+                Proceed to website
+              </button>
+            </p>
+            <p>
+              Looking to connect professionally? Drop a text on LinkedIn.
+            </p>
+            <button className="btn btn-primary">
+              <a
+                href="https://www.linkedin.com/in/rahulschandak/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="wd-button-font"
+              >LinkedIn</a>
+            </button>
+          </div>
+        </div>
+      )}
     </ScreenSizeProvider>
   );
 }
