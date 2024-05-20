2fd74f3250fccce3444c254fbde60451a32bea34
Rahul Chandak <rahulschandak@gmail.com>
2024-05-20 09:26:24 -0400

    social media icons

--- src/pf-main/pages/homeScreen/home-screen.js
+++ src/pf-main/pages/homeScreen/home-screen.js
@@ -5,35 +5,52 @@ import "./home-screen.css";
 
 function HomeScreen() {
   const { isDesktop } = useContext(ScreenSizeContext);
+
+  const heading = "Hello World,";
+  const subHeading = "I am Rahul Chandak";
+  const subTitle = "A Master's in Computer Science student at NEU";
+
+  const socialMediaData = [
+    {
+      name: "LinkedIn",
+      source: "images/linkedIn.png",
+      link: "https://www.instagram.com/rahulschandak/",
+    },
+    {
+      name: "Github",
+      source: "images/github.png",
+      link: "https://www.github.com/rahulschandak",
+    },
+    {
+      name: "Leetcode",
+      source: "images/LC.png",
+      link: "https://www.leetcode.com/u/rahulschandak/",
+    },
+    {
+      name: "Instagram",
+      source: "images/instagram.png",
+      link: "https://www.instagram.com/rahul_chandak/",
+    },
+  ];
+
   return isDesktop ? (
     <div className="page-container">
       <div className="text-container">
-        <BodyLabel className="h2">Hello World,</BodyLabel>
-        <BodyLabel className="h2">I am Rahul Chandak</BodyLabel>
-        <BodyLabel className="body-text padding-top">
-          A Master's in Computer Science student at NEU
+        <BodyLabel className="h2">{heading}</BodyLabel>
+        <BodyLabel className="h2">{subHeading}</BodyLabel>
+        <BodyLabel className="body-text-small padding-top">
+          {subTitle}
         </BodyLabel>
         <div className="social-media-container">
-          <img
-            src="images\instagram.png"
-            className="social-media-style"
-            alt="Instagram"
-          />
-          <img
-            src="images\twitter.png"
-            className="social-media-style"
-            alt="Twitter"
-          />
-          <img
-            src="images\github.png"
-            className="social-media-style"
-            alt="GitHub"
-          />
-          <img
-            src="images\linkedIn.png"
-            className="social-media-style"
-            alt="LinkedIn"
-          />
+          {socialMediaData.map((socialMedia) => (
+            <a href={socialMedia.link} target="_blank" rel="noreferrer">
+              <img
+                src={socialMedia.source}
+                className="social-media-style"
+                alt={socialMedia.name}
+              />
+            </a>
+          ))}
         </div>
         <a href="https://tinyurl.com/rscBio" target="_blank" rel="noreferrer">
           <button className="button-style"> Resume </button>
@@ -57,32 +74,21 @@ function HomeScreen() {
         />
       </div>
       <div className="text-container-mobile">
-        <BodyLabel className="h3">Hello World,</BodyLabel>
-        <BodyLabel className="h3">I am Rahul Chandak</BodyLabel>
-        <BodyLabel className="body-text padding-top">
-          A Master's in Computer Science student at NEU
+        <BodyLabel className="h3">{heading}</BodyLabel>
+        <BodyLabel className="h3">{subHeading}</BodyLabel>
+        <BodyLabel className="body-text-small padding-top">
+          {subTitle}
         </BodyLabel>
         <div className="social-media-container">
-          <img
-            src="images\instagram.png"
-            className="social-media-style"
-            alt="Instagram"
-          />
-          <img
-            src="images\twitter.png"
-            className="social-media-style"
-            alt="Twitter"
-          />
-          <img
-            src="images\github.png"
-            className="social-media-style"
-            alt="GitHub"
-          />
-          <img
-            src="images\linkedIn.png"
-            className="social-media-style"
-            alt="LinkedIn"
-          />
+          {socialMediaData.map((socialMedia) => (
+            <a href={socialMedia.link} target="_blank" rel="noreferrer">
+              <img
+                src={socialMedia.source}
+                className="social-media-style"
+                alt={socialMedia.name}
+              />
+            </a>
+          ))}
         </div>
         <button className="button-style"> Resume </button>
       </div>
